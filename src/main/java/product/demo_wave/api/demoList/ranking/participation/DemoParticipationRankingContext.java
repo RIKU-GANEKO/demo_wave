a623d48f0428196c5d8035aa8588bc25eb287ca4
RikuGaneko <ganeko@craid-inc.com>
2025-07-05 22:05:03 +0900

    レポートタブ

---
+++ src/main/java/product/demo_wave/api/demoList/ranking/participation/DemoParticipationRankingContext.java
@@ -0,0 +1,75 @@
+package product.demo_wave.api.demoList.ranking.participation;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import product.demo_wave.common.api.APIResponse;
+import product.demo_wave.common.api.ErrorCodeResponse;
+
+/**
+ * ユーザ情報取得用 Service
+ */
+@Builder
+public class DemoParticipationRankingContext {
+
+//	private static final Logger logger = Logger.getLogger(DemoListContext.class.getSimpleName());
+
+	@Getter
+	private final String firebaseUid;
+	private final String demoDate;
+
+	@Setter
+	private DemoParticipationRankingDBLogic demoParticipationRankingDBLogic;
+
+	/**
+	 * エラーレスポンスを生成して返す。
+	 *
+	 * @param error エラーの種類に対応する例外オブジェクト
+	 * @param errorDescription エラーの詳細
+	 * @param httpStatus HTTPステータスコード
+	 * @return エラーレスポンスを示すResponseEntityオブジェクト
+	 */
+	ResponseEntity<APIResponse> errorResponse(String error, String errorDescription, HttpStatus httpStatus) {
+		APIResponse errorResponse = new ErrorCodeResponse(error, errorDescription);
+		return new ResponseEntity<>(errorResponse, httpStatus);
+	}
+
+	/**
+	 * レスポンスに含む内容をDBLogicから生成し、APIレスポンスを返す。
+	 *
+	 * @return APIレスポンスを含むResponseEntityオブジェクト
+	 */
+	ResponseEntity<APIResponse> getDemoList() {
+		List<DemoParticipationRankingRecord> responses = null;
+
+		LocalDateTime startDateTime = null;
+		LocalDateTime endDateTime = null;
+
+		if (demoDate != null && !demoDate.isEmpty()) {
+			try {
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM");
+				YearMonth yearMonth = YearMonth.parse(demoDate, formatter);
+				LocalDate startDate = yearMonth.atDay(1);
+				LocalDate endDate = yearMonth.atEndOfMonth();
+				startDateTime = startDate.atStartOfDay();
+				endDateTime = endDate.atTime(23, 59, 59, 999_999_999);
+			} catch (Exception e) {
+				throw new IllegalArgumentException("日付形式が正しくありません。例: 2025/06", e);
+			}
+		}
+
+		responses = demoParticipationRankingDBLogic.fetchDemoList(startDateTime, endDateTime);
+
+		return new ResponseEntity<>(new DemoParticipationRankingResponse(responses), HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/product/demo_wave/api/demoList/ranking/participation/DemoParticipationRankingRecord.java
@@ -0,0 +1,21 @@
+package product.demo_wave.api.demoList.ranking.participation;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record DemoParticipationRankingRecord(
+		Integer id, // demo.id
+		String title, // demo.title
+		String content, // demo.content
+		String categoryImageUrl,
+		LocalDateTime demoStartDate, // demo.demoStartDate
+		LocalDateTime demoEndDate, // demo.demoEndDate
+		String demoPlace, // demo.demoPlace
+		BigDecimal demoAddressLatitude, // demo.demoAddressLatitude
+		BigDecimal demoAddressLongitude, // demo.demoAddressLongitude
+		String organizerUserName, // demo.user.name
+		String hostProfileImagePath, // 各デモの主催者のプロフィール画像
+		Long numberOfDemoParticipants, // 各demoの参加者合計数
+		BigDecimal totalDonatedMoney // 各demoの支援金総額
+) {
+}
